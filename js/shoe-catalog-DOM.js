57a38440bb76090d201c4fc36ef2a38558359f08
Dyllan Hope <dyllanjhope@gmail.com>
2019-05-16 14:38:51 +0200

    layout of wed page made

---
+++ js/shoe-catalog-DOM.js
@@ -0,0 +1,28 @@
+var dropDownTemplateSource = document.querySelector(".dropDownTemplate").innerHTML;
+var colourData = document.querySelector(".colourData");
+var brandData = document.querySelector(".brandData");
+var sizeData = document.querySelector(".sizeData");
+var dropDownTemplate = Handlebars.compile(dropDownTemplateSource);
+var shoeInstance = ShoeCatalogManager();
+
+window.onload = function(){
+    buildColourDropDown();
+    buildBrandDropDown();
+    buildsizeDropDown();
+}
+
+function buildColourDropDown(){
+    var colourOptions = { list: shoeInstance.colours()};
+    var colourHTML = dropDownTemplate(colourOptions);
+    colourData.innerHTML = colourHTML;
+}
+function buildBrandDropDown(){
+    var brandOptions = { list: shoeInstance.brand()};
+    var brandHTML = dropDownTemplate(brandOptions);
+    brandData.innerHTML = brandHTML;
+}
+function buildsizeDropDown(){
+    var sizeOptions = { list: shoeInstance.size()};
+    var sizeHTML = dropDownTemplate(sizeOptions);
+    sizeData.innerHTML = sizeHTML;
+}
